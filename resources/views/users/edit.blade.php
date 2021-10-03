8b756c949e852d543fc20cafe20bf80725d6b108
Kiran Anand <liteninkiran@hotmail.com>
2021-10-03 20:19:57 +0100

    Adding comments

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -6,28 +6,40 @@
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Users</h1>
     </div>
+
     <div class="container">
+
         <div class="row justify-content-center">
+
             <div class="col-md-8">
+
                 <div class="card">
+
+                    {{-- Sub Heading --}}
                     <div class="card-header">
+
                         {{ __('Update User') }}
+
+                        {{-- Back Button --}}
                         <a href="{{ route('users.index') }}" class="float-right">Back</a>
+
                     </div>
 
                     <div class="card-body">
+
+                        {{-- Form --}}
                         <form method="POST" action="{{ route('users.update', $user->id) }}">
+
+                            {{-- Tokens --}}
                             @csrf
                             @method('PUT')
+
+                            {{-- Username --}}
                             <div class="form-group row">
-                                <label for="username"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+                                <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="username" type="text"
-                                        class="form-control @error('name') is-invalid @enderror" name="username"
-                                        value="{{ old('username', $user->username) }}" required autocomplete="username"
-                                        autofocus>
+                                    <input id="username" type="text" class="form-control @error('name') is-invalid @enderror" name="username" value="{{ old('username', $user->username) }}" required autocomplete="username" autofocus>
 
                                     @error('username')
                                         <span class="invalid-feedback" role="alert">
@@ -37,15 +49,12 @@
                                 </div>
                             </div>
 
+                            {{-- First Name --}}
                             <div class="form-group row">
-                                <label for="first_name"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('First Name') }}</label>
+                                <label for="first_name" class="col-md-4 col-form-label text-md-right">{{ __('First Name') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="first_name" type="text"
-                                        class="form-control @error('name') is-invalid @enderror" name="first_name"
-                                        value="{{ old('first_name', $user->first_name) }}" required
-                                        autocomplete="first_name" autofocus>
+                                    <input id="first_name" type="text" class="form-control @error('name') is-invalid @enderror" name="first_name" value="{{ old('first_name', $user->first_name) }}" required autocomplete="first_name" autofocus>
 
                                     @error('first_name')
                                         <span class="invalid-feedback" role="alert">
@@ -55,15 +64,12 @@
                                 </div>
                             </div>
 
+                            {{-- Last Name --}}
                             <div class="form-group row">
-                                <label for="last_name"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Last Name') }}</label>
+                                <label for="last_name" class="col-md-4 col-form-label text-md-right">{{ __('Last Name') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="last_name" type="text"
-                                        class="form-control @error('name') is-invalid @enderror" name="last_name"
-                                        value="{{ old('last_name', $user->last_name) }}" required
-                                        autocomplete="last_name" autofocus>
+                                    <input id="last_name" type="text" class="form-control @error('name') is-invalid @enderror" name="last_name" value="{{ old('last_name', $user->last_name) }}" required autocomplete="last_name" autofocus>
 
                                     @error('last_name')
                                         <span class="invalid-feedback" role="alert">
@@ -73,13 +79,12 @@
                                 </div>
                             </div>
 
+                            {{-- Email --}}
                             <div class="form-group row">
-                                <label for="email"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
-                                        name="email" value="{{ old('email', $user->email) }}">
+                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email', $user->email) }}">
 
                                     @error('email')
                                         <span class="invalid-feedback" role="alert">
@@ -88,6 +93,8 @@
                                     @enderror
                                 </div>
                             </div>
+
+                            {{-- Submit Button --}}
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
@@ -95,32 +102,55 @@
                                     </button>
                                 </div>
                             </div>
+
                         </form>
+
                     </div>
+
                 </div>
+
+                {{-- Delete User --}}
                 <div class="m-2 p-2">
+
+                    {{-- Form --}}
                     <form method="POST" action="{{ route('users.destroy', $user->id) }}">
+
+                        {{-- Tokens --}}
                         @csrf
                         @method('DELETE')
+
+                        {{-- Submit Button --}}
                         <button class="btn btn-danger">Delete {{ $user->username }}</button>
+
                     </form>
+
                 </div>
+
             </div>
+
+            {{-- Change Password --}}
             <div class="col-md-8">
+
                 <div class="card">
+
+                    {{-- Sub Heading --}}
                     <div class="card-header">{{ __('Change Password') }}</div>
 
                     <div class="card-body">
+
+                        {{-- Form --}}
                         <form method="POST" action="{{ route('users.change.password', $user->id) }}">
+
+                            {{-- Token --}}
                             @csrf
+
+                            {{-- Password --}}
                             <div class="form-group row">
-                                <label for="password"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="password" type="password"
-                                        class="form-control @error('password') is-invalid @enderror" name="password"
-                                        autocomplete="new-password">
+                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" autocomplete="new-password">
 
                                     @error('password')
                                         <span class="invalid-feedback" role="alert">
@@ -130,16 +160,16 @@
                                 </div>
                             </div>
 
+                            {{-- Confirm Password --}}
                             <div class="form-group row">
-                                <label for="password-confirm"
-                                    class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
                                 <div class="col-md-6">
-                                    <input id="password-confirm" type="password" class="form-control"
-                                        name="password_confirmation" autocomplete="new-password">
+                                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" autocomplete="new-password">
                                 </div>
                             </div>
 
+                            {{-- Submit Button --}}
                             <div class="form-group row mb-0">
                                 <div class="col-md-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
@@ -147,10 +177,17 @@
                                     </button>
                                 </div>
                             </div>
+
                         </form>
+
                     </div>
+
                 </div>
+
             </div>
+
         </div>
+
     </div>
+
 @endsection
