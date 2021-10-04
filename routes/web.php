0b7629d701f29f5fabe1255c2193d6b17d6dc4fd
Kiran Anand <liteninkiran@hotmail.com>
2021-10-04 17:55:47 +0100

    Adding route

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\Backend\UserController;
 use App\Http\Controllers\Backend\CountryController;
 use App\Http\Controllers\Backend\StateController;
+use App\Http\Controllers\Backend\CityController;
 use App\Http\Controllers\Backend\ChangePasswordController;
 
 /*
@@ -28,5 +29,6 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::resource('users', UserController::class);
 Route::resource('countries', CountryController::class);
 Route::resource('states', StateController::class);
+Route::resource('cities', CityController::class);
 
 Route::post('users/{user}/change-password', [ChangePasswordController::class, 'change_password'])->name('users.change.password');
